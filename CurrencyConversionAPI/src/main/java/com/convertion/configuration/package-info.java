4814c2d4097972235c0d8337b37e7c0d86b3f769
KAKANAKOU Miguel Stephane <because_of_peace@yahoo.com>
2017-04-21 16:07:15 +0800

    Add the java configuration of the project

---
+++ CurrencyConversionAPI/src/main/java/com/convertion/configuration/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the java configuration of the project.
+ * 
+ * @author KAKANAKOU Miguel Stephane (Skakanakou@gmail.com)
+ *
+ */
+package com.convertion.configuration;
---
+++ CurrencyConversionAPI/src/main/java/com/convertion/repositories/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the repositories
+ * 
+ * @author KAKANAKOU Miguel Stephane (Skakanakou@gmail.com)
+ *
+ */
+package com.convertion.repositories;
