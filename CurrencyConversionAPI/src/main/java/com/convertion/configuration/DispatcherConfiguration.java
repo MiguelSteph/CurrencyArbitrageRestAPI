fddfe4374c7417442767ce12c525cb9cf5036ba5
KAKANAKOU Miguel Stephane <because_of_peace@yahoo.com>
2017-04-22 07:56:12 +0800

    Update the project configuration

--- CurrencyConversionAPI/src/main/java/com/convertion/configuration/DispatcherConfiguration.java
+++ CurrencyConversionAPI/src/main/java/com/convertion/configuration/DispatcherConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableScheduling
-@ComponentScan(basePackages = { "com.convertion.repositories" })
+@ComponentScan(basePackages = { "com.convertion.repositories", "com.convertion.services" })
 public class DispatcherConfiguration extends WebMvcConfigurerAdapter {
     
 }
